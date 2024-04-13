01a77032cb7219e196b238f4a7157f60184cd4fc
3K-runner <144951500+3K-runner@users.noreply.github.com>
2024-04-13 00:26:33 -0300

    Support for puzzle strings (#2)
    * Added new feature "input puzzle strings"
    
    * Improved website responsiveness
    
    * Pieces list improved

--- board.js
+++ board.js
@@ -1,45 +1,52 @@
-const PIECE_LIST = "pieces" + BOARD_NUM.toString();
-
-const Kanji = listname => {
-    switch (listname) {
-    case "pieces9": return [" ", "歩", "香", "桂", "銀", "金", "角", "飛", "王"]; break;
-    case "pieces5": return [" ", "歩", "銀", "金", "角", "飛", "王"]; break;
-    default: return []; break;
+const Kanji = num => {
+    switch (num) {
+    case 9:  return [[" ", " "], 
+                     ["歩", "と"], 
+                     ["香", "杏"], 
+                     ["桂", "圭"], 
+                     ["銀", "全"], 
+                     ["金", "金"], 
+                     ["角", "馬"], 
+                     ["飛", "竜"], 
+                     ["王", "玉"]];
+    case 5:  return [[" ", " "], 
+                     ["歩", "と"], 
+                     ["銀", "全"], 
+                     ["金", "金"], 
+                     ["角", "馬"], 
+                     ["飛", "竜"], 
+                     ["王", "玉"]];
+    default: return [];
     };
 };
 
-const proPieces = listname => {
-    switch (listname) {
-    case "pieces9": return [" ", "と", "杏", "圭", "全", "金", "馬", "竜", "王"]; break;
-    case "pieces5": return [" ", "と", "全", "金", "馬", "竜", "王"]; break;
-    default: return []; break;
-    };
-};
+const pieces = Kanji(BOARD_NUM);
 
-const pieces = Kanji(PIECE_LIST);
-const propieces = proPieces(PIECE_LIST);
-
-var std_tab_var = [];
-for (let i = 0; i < BOARD_NUM; i++) {
-    std_tab_var[i] = [];
-    for (let j = 0; j < BOARD_NUM; j++) {
-        std_tab_var[i][j] = [0, 0, 0];
+const makeTab = () => {
+    let std_tab_var = [];
+    for (let i = 0; i < BOARD_NUM; i++) {
+        std_tab_var[i] = [];
+        for (let j = 0; j < BOARD_NUM; j++) {
+            std_tab_var[i][j] = [0, 0, 0];
+        }
     }
+    return std_tab_var;
 }
-const std_tab = std_tab_var;
 
-var std_hand_var = [];
-for (let i = 0; i < 2; i++) {
-    std_hand_var[i] = [];
-    for (let j = 0; j < pieces.length - 1; j++) {
-        std_hand_var[i][j] = 0;
+const makeHand = () => {
+    let std_hand_var = [];
+    for (let i = 0; i < 2; i++) {
+        std_hand_var[i] = [];
+        for (let j = 0; j < pieces.length - 1; j++) {
+            std_hand_var[i][j] = 0;
+        }
     }
+    return std_hand_var;
 }
-const std_hand = std_hand_var;
 
-const clean_board = ({
-    tab: std_tab,
-    hand: std_hand,
+const clean_board = () => ({
+    tab:  makeTab(),
+    hand: makeHand(),
     turn: 0,
     mate: 0,
 });
@@ -49,48 +56,43 @@ const clean_board = ({
 const FillBoard = boardToUse => {
     for (let i = 0; i < BOARD_NUM; i++) {
         for (let j = 0; j < BOARD_NUM; j++) {
-            var piece = boardToUse.tab[i][j][0];
-            var tile = document.getElementById(tileId(i, j) + "t");
-            if (piece == 0) continue;
-               
-            if ((boardToUse.tab[i][j][1] == 0)) {
-                tile.style.transform = "rotate(180deg)";  
-            }
-            if (boardToUse.tab[i][j][2] == 0) {
-                tile.textContent = pieces[piece];
-                tile.style.color = "#000000";
-            } else {
-                tile.textContent = propieces[piece];
-                tile.style.color = "#FF0000";
-            }
+            const piece = boardToUse.tab[i][j];
+            let tile = document.getElementById(tileId(i, j) + "t");
+            
+            const turn_or_pro = (piece[0] == (pieces.length - 1)) ? 1 : 2;
+            tile.textContent = pieces[piece[0]][piece[turn_or_pro]];
+            if (piece[0] == 0) continue;
+            
+            tile.style.transform = (piece[1] == 0) ? "rotate(180deg)" : "" ;
+            tile.style.color     = (piece[2] == 0) ? "#000000" : "#FF0000";
         }
     }
 }
 
 const handId = turn => "hand" + turn.toString();
 
-const FillHand = boardToUse =>{
+const FillHand = boardToUse => {
     for (let i = 0; i < 2; i++) {
-        var handstr = "";
-        if (boardToUse.hand[i][0] == 0) continue;
-        for  (let j = 1; j < pieces.length - 1; j++) {
-            var numhand = boardToUse.hand[i][j];
-            if (numhand == 0) continue;
-            handstr = handstr + numhand.toString() + pieces[j] + " ";
+        let handstr = "";
+        if (boardToUse.hand[i][0] != 0) {
+            for (let j = 1; j < pieces.length - 1; j++) {
+                let numhand = boardToUse.hand[i][j];
+                if (numhand == 0) continue;
+                handstr = handstr + numhand.toString() + pieces[j][0] + " ";
+            }
         }
-        if (handstr.length == 0) continue;
 
         document.getElementById(handId(i)).textContent = handstr;
     }
 }
 
 const setMate = (turn, mate) => {
-    const player = (turn == 0) ? "Sente" : "Gote";
+    const player = (turn == 0) ? "Gote" : "Sente";
     const mateMessage = "Mate in " + mate.toString() + ": " + player + " to win";
     document.getElementById("mate").textContent = mateMessage;
 }
 
-const boardfile = ({
+const boardfile = () => ({
     file: "",
     count: 0,
 
@@ -99,17 +101,17 @@ const boardfile = ({
     },
 
     Read() {
-        let resp = this.file[this.count];
-        this.count = this.count + 1;
+        const resp = this.file[this.count];
+        this.count++;
         return Number(resp);
     }
 });
 
 const setBoard = boardstr => board => {
-    var boardTxt = boardfile;
+    let boardTxt = boardfile();
     boardTxt.Add(boardstr);
 
-    var buff = 0;
+    let buff = 0;
     for (let i = 0; i < 2; i++) {
         if (boardTxt.Read() == 0) continue;
         board.hand[i][0] = 1;
@@ -133,7 +135,7 @@ const setBoard = boardstr => board => {
 }
 
 const run = boardstr => {
-    var board = clean_board;
+    let board = clean_board();
 
     if (boardstr.length > 0) {
         setBoard(boardstr)(board);
@@ -144,12 +146,37 @@ const run = boardstr => {
     setMate(board.turn, board.mate);
 }
 
-const boardstr = "101000200200000060011041100000041110000002006105105000011";
+let boardstr = "101000200200000060011041100000041110000002006105105000011";
 
 //01010004110400310000021060010000005106105010031000010114
 //01100000000600211300004002100100005110006103000400510014
 //101000200200000060011041100000041110000002006105105000011
 
+//shortest plausible str: 00000000000000000000000000000
+
 run(boardstr);
+
+const onlyAllowedChars = char => {
+    const allowedChar = [..."1234567890"];
+    const checkfunc = (result, element) => result || (char == element);
+    return allowedChar.reduce(checkfunc, false);
+}
+
+const verifyInputStr = str => {
+    if (str == "0") return [];
+
+    const clean_str = [...str].filter(onlyAllowedChars);
+    if (clean_str.length > 0) {
+        boardstr = clean_str;
+    }
+    return boardstr
+}
+
+const getInputStr = element => {
+    element.preventDefault();
+    const str = element.currentTarget.Input.value;
+    run(verifyInputStr(str));
+}
+document.getElementById("input").addEventListener("submit", getInputStr);
 
 //B1bG1/3Sk/p4/RK+r2/G3+p_b_-_1
